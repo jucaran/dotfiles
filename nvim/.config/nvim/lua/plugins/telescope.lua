7d8aac879605431e7b064ec0945210528fbebe6e
Jucaran <juan.castro.arancibia@gmail.com>
2025-02-05 18:37:10 -0300

    add custom key for telescope preview scroll

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,48 +1,56 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" }
-    },
-    config = function()
-      local telescope = require("telescope")
-      local telescope_actions = require("telescope.actions")
-      local builtin = require("telescope.builtin")
-      local lga_actions = require("telescope-live-grep-args.actions")
-      vim.keymap.set("n", "<leader>p", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", telescope.extensions.live_grep_args.live_grep_args, {})
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{ "nvim-telescope/telescope-live-grep-args.nvim", version = "^1.0.0" },
+		},
+		config = function()
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+			local builtin = require("telescope.builtin")
+			local lga_actions = require("telescope-live-grep-args.actions")
 
-      telescope.setup({
-        extensions = {
-          live_grep_args = {
-            auto_quoting = true,
-            mappings = {
-              i = {
-                ["<C-k>"] = lga_actions.quote_prompt(),
-                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-              },
-            },
-          }
-        }
-      })
+			vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", telescope.extensions.live_grep_args.live_grep_args, {})
 
-      require("telescope").load_extension("live_grep_args")
-      require("telescope").load_extension("ui-select")
-    end
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown {
-            },
-          }
-        }
-      })
-    end
-  }
+			telescope.setup({
+				defaults = {
+					mappings = {
+						n = {
+							["J"] = actions.preview_scrolling_down,
+							["K"] = actions.preview_scrolling_up,
+						},
+					},
+				},
+				extensions = {
+					live_grep_args = {
+						auto_quoting = true,
+						mappings = {
+							i = {
+								["<C-k>"] = lga_actions.quote_prompt(),
+								["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+							},
+						},
+					},
+				},
+			})
+
+			require("telescope").load_extension("live_grep_args")
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+		end,
+	},
 }
