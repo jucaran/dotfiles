9cc27346ada02dcbbf28b4cad41e1a031d978871
Juan Castro Arancibia <juan.castro.arancibia@gmail.com>
2025-02-05 12:42:48 -0300

    Adds maximazing tabs mappings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -3,6 +3,9 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.cmd("set number")
+vim.keymap.set("n", "<C-W>M", "<C-W>|<C-W>_", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-W>m", "<C-W>=", { noremap = true, silent = true })
 
 vim.g.mapleader = " "
 
